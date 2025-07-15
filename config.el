dcb17c920d800138b729e728309a59582dc60178
rpourteau <remy.pourteau@cosylab.com>
2025-07-15 11:31:12 +0200

    Do not automatically launch vhdl-lsp server on vhd files

--- config.el
+++ config.el
@@ -124,7 +124,7 @@
     :major-modes '(vhdl-mode)
     :server-id 've-rust-hdl)))
 
-(add-hook! vhdl-mode #'lsp!)
+;;(add-hook! vhdl-mode #'lsp!)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
