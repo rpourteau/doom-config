20d0f819db8d599faec83dad34ddcbaf975712ef
rpourteau <remy.pourteau@cosylab.com>
2025-08-19 10:22:58 +0200

    Remove some useless comments

--- config.el
+++ config.el
@@ -21,26 +21,10 @@
 (setq user-full-name "rpourteau"
       user-mail-address "remy.pourteau@cosylab.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept.
+;; Themes and font
+;; JetBrains Mono without ligatures
 (setq doom-font (font-spec :family "JetBrains Mono NL" :size 18))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -54,6 +38,7 @@
          ("<f3>"   . highlight-symbol-next)
          ("S-<f3>" . highlight-symbol-prev)
          ("M-<f3>" . highlight-symbol-query-replace)))
+
 (use-package! systemrdl-mode
   :mode ("\\.rdl\\'" . systemrdl-mode)
   :custom
