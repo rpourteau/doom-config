6a349af9efb0b3e5c33f36cf200874dbb841f780
rpourteau <remy.pourteau@cosylab.com>
2025-09-08 11:18:29 +0200

    Add comments in package

--- packages.el
+++ packages.el
@@ -5,15 +5,19 @@
 (package! systemrdl-mode
   :recipe (:host github :repo "paul-donahue/systemrdl-mode"))
 
+;; Required to support LSP on VHDL
 (package! vhdl-ext
   :recipe (:host github :repo "gmlarumbe/vhdl-ext"))
 
+;; Better integrated symbol highlight
 (package! symbol-overlay
   :recipe (:host github :repo "wolray/symbol-overlay"))
 
+;; Claude Code IDE
 (package! claude-code-ide
   :recipe (:host github :repo "manzaltu/claude-code-ide.el"))
 
+;; EAT terminal easier than vterm (do not need to compile C library)
 (straight-use-package
  '(eat :type git
        :host codeberg
